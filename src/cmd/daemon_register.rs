87a89af2a68462e0b50562e6c28953c6137d6a45
Dubzer <me@dubzer.dev>
2024-08-29 22:38:35 +0300

    fix: improve service registration message

--- src/cmd/daemon_register.rs
+++ src/cmd/daemon_register.rs
@@ -2,13 +2,20 @@ use color_eyre::eyre::eyre;
 use objc2_foundation::ns_string;
 use objc2_service_management::SMAppService;
 
+use crate::constants::SOCKET_PATH;
+
 pub unsafe fn cmd_daemon_register() -> color_eyre::Result<()> {
     let name = ns_string!("launchd.plist");
     let service = SMAppService::agentServiceWithPlistName(name);
     
     let _ = service.unregisterAndReturnError();
     match service.registerAndReturnError() {
-        Ok(_) => Ok(println!("Service registered successfully")),
+        Ok(_) => {
+            println!("Service registered successfully");
+            println!("To get started, add the following to your ~/.ssh/config file:");
+            println!("Host *\n  IdentityAgent \"{}\"", &*SOCKET_PATH.clone().to_string_lossy());
+            Ok(())
+        },
         Err(e) => Err(eyre!(e.localizedDescription().to_string())),
     }
 }
